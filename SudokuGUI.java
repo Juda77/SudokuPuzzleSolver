5ac16c8d81e5ac434d8007da6ebc476eab35c3dd
Toadsmen77 <finn.toadsmen@gmail.com>
2021-03-28 04:45:31 -0700

    First commit

---
+++ SudokuGUI.java
@@ -0,0 +1,125 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SudokuGUI {
+
+	
+	public static void main(String[] args) {
+		
+		SudokuGUI gui = new SudokuGUI();
+		gui.setupGUI(new int[9][9], gui);
+	}
+	
+	
+	public void setupGUI(int[][] board, SudokuGUI gui) {
+	
+		JFrame frame = new JFrame("Sudoku Solver"); //main window
+		JPanel panel = new JPanel();
+		JTextField[][] textFields = new JTextField[9][9];
+		
+		panel.setLayout(new GridLayout(10, 9));
+		for (int row = 0; row < 9; row++) {
+			for (int col = 0; col < 9; col++) {
+				textFields[row][col] = new JTextField();
+				panel.add(textFields[row][col]);
+			}
+		}
+		
+		JButton submitButton = new JButton("Solve this Sudoku puzzle!");
+		panel.add(submitButton);
+		frame.add(panel);
+		
+		frame.setSize(500, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //activates the red cross button
+		frame.setVisible(true); //makes JFrame visible
+		
+		//if the submit button is pressed, take all input from the textfields 
+		submitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				if (event.getSource() == submitButton) {
+					
+					boolean givenValidPuzzle = true;
+					for (int row = 0; row < 9; row++) {
+						for (int col = 0; col < 9; col++) {
+							
+							String textFieldValue = textFields[row][col].getText();
+							int currCell = 0;
+							try {
+	
+								currCell = Integer.parseInt(textFieldValue);
+								
+							} catch(Exception e) {
+								
+								givenValidPuzzle = false;
+								System.out.println("hi");
+								System.out.println("Cell " + row + ", " + col + " is not a valid number");							
+								
+							}
+							
+							if (currCell < 0 || 10 <= currCell) {
+								
+								givenValidPuzzle = false;
+								System.out.println("hi");
+								System.out.println("Cell " + row + ", " + col + " is not a valid number");	
+							} else {
+								board[row][col] = currCell;
+							}
+							
+						}
+	
+					}
+					
+					if (givenValidPuzzle) {
+						SudokuSolver.solveSudoku(board, gui);
+					}
+					
+					
+				}
+				
+			}
+	
+		});	
+		
+	}
+	
+	//method which presents user with the solved sudoku board
+	public void presentSolvedPuzzleGUI(int[][] board) {
+		
+		JFrame frame = new JFrame("Sudoku Puzzle Solved"); //main window
+		JPanel panel = new JPanel();
+		
+		panel.setLayout(new GridLayout(10, 9));
+		for (int row = 0; row < 9; row++) {
+			for (int col = 0; col < 9; col++) {
+				
+				JTextField textField = new JTextField();
+				textField.setText(board[row][col] + "");
+				panel.add(textField);
+			}
+		}
+		
+		frame.add(panel);
+		
+		frame.setSize(500, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //activates the red cross button
+		frame.setVisible(true); //makes JFrame visible
+		
+		
+	}
+	
+	
+
+
+}
---
+++ SudokuSolver.java
@@ -0,0 +1,141 @@
+import java.util.HashSet;
+
+public class SudokuSolver {
+ 
+    static HashSet<Integer>[] rowSets = new HashSet[9];
+    static HashSet<Integer>[] colSets = new HashSet[9];
+    static HashSet<Integer>[] boxSets = new HashSet[9];
+    
+    /*
+     * Good CSS professors:
+     * Dimpsey
+     * Pisan
+     * Bernstein
+     * Dupuis
+     * Chenault
+     */
+    
+    
+    
+    public static void main(String[] args) {}
+
+    public static void solveSudoku(int[][] board, SudokuGUI gui) {
+        
+        initializeSets(board);
+        
+        boolean sudokuPuzzleIsSolvable = traverseBoard(board, 0, 0);
+        if (sudokuPuzzleIsSolvable) {
+  
+        	gui.presentSolvedPuzzleGUI(board);
+
+        } else {
+            System.out.println("Sudoku puzzle is not solvable");
+        }
+ 
+    }
+    
+    private static boolean traverseBoard(int[][] board, int row, int col) {
+        
+        if (row == 9) {
+            return true;
+        } else if (col == 9) {
+            return traverseBoard(board, row + 1, 0);
+        } else if (board[row][col] != 0) {
+            return traverseBoard(board, row, col + 1);
+        } else if (board[row][col] == 0) {
+            
+            int boxIndex = determineBox(row, col);
+            //find a valid current spot
+            for (int numChoice = 1; numChoice <= 9; numChoice++) {
+    
+                boolean numChoiceIsValid = !rowSets[row].contains(numChoice) 
+                    && !colSets[col].contains(numChoice) 
+                    && !boxSets[boxIndex].contains(numChoice);
+                
+                boolean allChoicesBeyondAreValid = false;
+                if (numChoiceIsValid) {
+                    board[row][col] = numChoice;
+                    rowSets[row].add(numChoice);
+                    colSets[col].add(numChoice);
+                    boxSets[boxIndex].add(numChoice);
+                    allChoicesBeyondAreValid = traverseBoard(board, row, col + 1);
+                    if (allChoicesBeyondAreValid) {
+                        return true;
+                    } else {
+                        board[row][col] = 0;
+                        rowSets[row].remove(numChoice);
+                        colSets[col].remove(numChoice);
+                        boxSets[boxIndex].remove(numChoice);
+
+                    }
+                }
+                
+            }
+                       
+        }
+        
+        return false;
+    	
+    }
+
+    private static int determineBox(int row, int col) {
+
+        boolean rowIsIn1stTriSection = row <= 2;
+        boolean rowIsIn2ndTriSection = 3 <= row && row <= 5;
+        boolean rowIsIn3rdTriSection = 6 <= row;
+
+        boolean colIsIn1stTriSection = col <= 2;
+        boolean colIsIn2ndTriSection = 3 <= col && col <= 5;
+        boolean colIsIn3rdTriSection = 6 <= col;
+
+        if (rowIsIn1stTriSection && colIsIn1stTriSection) {
+            return 0;
+        } else if (rowIsIn1stTriSection && colIsIn2ndTriSection) {
+            return 1;
+        } else if (rowIsIn1stTriSection && colIsIn3rdTriSection) {
+            return 2;
+        } else if (rowIsIn2ndTriSection && colIsIn1stTriSection) {
+            return 3;
+        } else if (rowIsIn2ndTriSection && colIsIn2ndTriSection) {
+            return 4;
+        } else if (rowIsIn2ndTriSection && colIsIn3rdTriSection) {
+            return 5;
+        } else if (rowIsIn3rdTriSection && colIsIn1stTriSection) {
+            return 6;
+        } else if (rowIsIn3rdTriSection && colIsIn2ndTriSection) {
+            return 7;
+        } else {
+            return 8;
+        }
+        
+    }
+
+    private static void initializeSets(int[][] board) {
+
+        for (int i = 0; i < 9; i++) {
+
+            rowSets[i] = new HashSet<Integer>();
+            colSets[i] = new HashSet<Integer>();
+            boxSets[i] = new HashSet<Integer>();
+
+        }
+    
+        for (int i = 0; i < board.length; i++) {
+    		for (int j = 0; j < board[i].length; j++) {
+    			
+    			int cell = board[i][j];
+    			if (cell != 0) {
+    				
+    				rowSets[i].add(cell);
+    				colSets[j].add(cell);
+    				boxSets[determineBox(i, j)].add(cell);
+    				
+    			}
+    			
+    		}
+    		
+    	}
+        
+    }
+
+}
